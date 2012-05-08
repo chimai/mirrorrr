1b4c436e5cb06d144daf582186bea7dba68d7dd2
liruqi <liruqi@gmail.com>
2012-05-08 14:49:32 +0800

    no redirect

--- mirror.py
+++ mirror.py
@@ -259,11 +259,6 @@ class MirrorHandler(BaseHandler):
     if translated_address == "favicon.ico":
       return self.redirect("/favicon.ico")
     mirrored_url = HTTP_PREFIX + translated_address
-    if self.request.user_agent.find("Android") == -1 and self.request.headers["Host"]=="opliruqi.appspot.com":
-      if base_url.split('.')[-1] != "com":
-        return self.redirect("http://liruqi001.appspot.com/" + translated_address)
-      if base_url.split('.')[-1] == "com":
-        return self.redirect("http://wcproxyx.appspot.com/" + translated_address)
 
     if base_url in HSTS_DOMAINS:
       mirrored_url = HTTPS_PREFIX + translated_address
